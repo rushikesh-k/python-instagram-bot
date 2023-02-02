002b56b7e5b7b88e83c5172ae14104bc9f9a6d4b
Rushikesh K <79825181+rushikesh-aist@users.noreply.github.com>
2023-02-02 10:58:39 +0700

    Login working OTP verification (in progress)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # python-instagram-bot
 Instagram bot with Auto login
+
+This python script can automatically login into your Instagram.
+
+# How to use:
+1. Install requirements using terminal or command line. You should run this command ```pip install -r requirements.txt```.
+2. Create a .env file in the current directory and add ig username and password as explained in the first comment in run.py
+3. Open a terminal or cmd again and run the bot using this command: ```python instagram.py```.
---
+++ instagram.py
@@ -0,0 +1,148 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import TimeoutException
+from decouple import config
+
+# Setup a .env file in the current directory
+# like
+# iguser=yourusername
+# igpassword=yourpsw
+# ==========================================
+USERNAME = config("iguser")
+PASSWORD = config("igpassword")
+# ==========================================
+
+TIMEOUT = 25
+
+
+def scrape():
+    usr = input('[Required] - Whose followers do you want to scrape: ')
+
+    user_input = int(
+        input('[Required] - How many followers do you want to scrape (60-500 recommended): '))
+
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
+
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('start-maximized')
+    options.add_argument('disable-infobars')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--log-level=3")
+    mobile_emulation = {
+        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/90.0.1025.166 Mobile Safari/535.19"}
+    options.add_experimental_option("mobileEmulation", mobile_emulation)
+
+    bot = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=options)
+
+    bot.get('https://www.instagram.com/accounts/login/')
+
+    time.sleep(1)
+
+    # Wait for the cookie policy button to be present
+    wait = WebDriverWait(bot, 10)
+    """ try:
+        cookie_policy_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div/div/div[3]/div[2]/button")))
+        bot.execute_script("window.scrollTo(0, document.body.scrollHeight)") 
+        cookie_policy_button.click()
+    except TimeoutException:
+        print("Cookie policy button not found on the page")
+
+    print("[Info] - Logging in...")   """
+
+    user_element = WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((
+            By.XPATH, '//*[@id="loginForm"]/div[1]/div[3]/div/label/input')))
+
+    user_element.send_keys(USERNAME)
+
+    print("[Info] - sent username...")
+
+    pass_element = WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((
+            By.XPATH, '//*[@id="loginForm"]/div[1]/div[4]/div/label/input')))
+
+    pass_element.send_keys(PASSWORD)
+
+    login_button = WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((
+            By.XPATH, '//*[@id="loginForm"]/div[1]/div[6]/button')))
+
+    time.sleep(0.4)
+
+    login_button.click()
+
+    time.sleep(10)
+
+    current_url = bot.current_url
+    if current_url == "https://www.instagram.com/accounts/login/two_factor?next=%2F":
+        print("Two-factor authentication is required")
+        otp = input('[Required] - Enter OTP: ')
+
+        # find matching dive element mount_0_0_
+        current_div_id = bot.find_element_by_xpath(
+            "//div[starts-with(@id, 'mount_0_0_')]").get_attribute("id")
+
+        otp_element = WebDriverWait(bot, TIMEOUT).until(
+            EC.presence_of_element_located((
+                By.XPATH, f'//*[@id="{current_div_id}"]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div/div/div[2]/form/div[1]/div/label/input')))
+        otp_element.send_keys(otp)
+        otp_button = WebDriverWait(bot, TIMEOUT).until(
+            EC.presence_of_element_located((
+                By.XPATH, '//*[@id="mount_0_0_I2"]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div/div/div[2]/form/div[2]/button')))
+        time.sleep(0.4)
+        otp_button.click()
+    else:
+        print("Two-factor authentication is not required")
+
+    time.sleep(10)
+
+    bot.get('https://www.instagram.com/{}/'.format(usr))
+
+    time.sleep(3.5)
+
+    WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((
+            By.XPATH, "//a[contains(@href, '/following')]"))).click()
+
+    time.sleep(2)
+
+    print('[Info] - Scraping...')
+
+    users = set()
+
+    for _ in range(round(user_input // 20)):
+
+        ActionChains(bot).send_keys(Keys.END).perform()
+
+        time.sleep(1)
+
+    followers = bot.find_elements(By.XPATH,
+                                  "//a[contains(@href, '/')]")
+
+    # Getting url from href attribute
+    for i in followers:
+        if i.get_attribute('href'):
+            users.add(i.get_attribute('href').split("/")[3])
+        else:
+            continue
+
+    print('[Info] - Saving...')
+    print('[DONE] - Your followers are saved in followers.txt file!')
+
+    with open('followers.txt', 'a') as file:
+        file.write('\n'.join(users) + "\n")
+
+
+if __name__ == '__main__':
+    scrape()
